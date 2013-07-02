6985beea16877b2178ca181dd0b2c6259f7ecd77
johnotander <johnotander@gmail.com>
2013-07-02 16:11:31 -0600

    Add layout.

--- uceem_api.rb
+++ uceem_api.rb
@@ -1,9 +1,16 @@
 require 'rubygems'
 require 'sinatra'
+require 'glorify'
 
 set :erb, format: :html5
-set :markdown, layout_engine: :erb, layout: false
+set :markdown, layout_engine: :erb, layout: :layout
+
+Tilt.prefer Sinatra::Glorify::Template
 
 get '/' do
   markdown :index
+end
+
+get '/authentication' do
+  markdown :authentication
 end
